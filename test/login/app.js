f0c76af3c37d48a07a69223673f089854dc5fa2e
ehsan-hashemi <1392ehsan1392ehsan@gmail.com>
2025-06-01 07:05:26 +0330

    UP Code

--- test/login/app.js
+++ test/login/app.js
@@ -7,6 +7,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     let password = document.getElementById("password").value.trim();
 
     const users = [
+        // دانش آموزان
         { 
             name: "سید محمد احسان هاشمی کلاگری",
             birth: "1392/01/21",
@@ -17,13 +18,27 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
             className: "دانش پژوهان ۲",
             url: "../dashbord/student/"
         },
+
+        // معلمان
+        { 
+            name: "حسن ملاعلی",
+            birth: "0000/00/01",
+            code: "0000000000",
+            pass: "0000000000",
+            grade: "ششم",
+            className: "دانش پژوهان ۲",
+            url: "../dashbord/teacher/"
+        },
+
+        // مدیران
         { 
             name: "حسن ملاعلی",
             birth: "0000/00/00",
             code: "0000000000",
             pass: "0000000000",
+            className: "مدیر مدرسه",
             url: "../admin"
-        }
+        },
     ];
 
     let user = users.find(u => u.name === fullName && u.birth === birthDate && u.code === nationalCode && u.pass === password);
